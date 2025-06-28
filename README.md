7432c3beb55b964c8c0b510147ceb4f0550c1268
Bojan Milevski <bojan@bojanmilevski.com>
2025-06-28 15:06:05 +0200

    nvim: manage `lazy.nvim` through configuration instead of system package manager

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # DOTFILES
 
 Various program configuration files.
-
-# NOTES
-
-- It it assumed that all required tools are installed through your system's
-  package manager.
-- I have the `lazy` package manager installed through my system package manager.
-- For the various programs you might need, please refer to [my list of programs
-  I use](https://github.com/bojanmilevski/scripts/blob/main/programs.txt).
--- src/.config/nvim/lua/config/settings.lua
+++ src/.config/nvim/lua/config/settings.lua
@@ -1,86 +1,83 @@
-local o = vim.o
-local opt = vim.opt
-
 -- column numbers
-o.number = true
-o.relativenumber = true
+vim.o.number = true
+vim.o.relativenumber = true
 
 -- indentation
 local spaces = 2
-o.autoindent = true
-o.shiftwidth = spaces
-o.smartindent = true
-o.softtabstop = spaces
-o.tabstop = spaces
-opt.expandtab = true
+vim.o.autoindent = true
+vim.o.shiftwidth = spaces
+vim.o.smartindent = true
+vim.o.softtabstop = spaces
+vim.o.tabstop = spaces
+vim.opt.expandtab = true
 
 -- scroll offset
 local scroll_offset = 8
-o.sidescrolloff = scroll_offset
-o.scrolloff = scroll_offset
-o.sidescroll = 1
+vim.o.sidescrolloff = scroll_offset
+vim.o.scrolloff = scroll_offset
+vim.o.sidescroll = 1
 
 -- backup
-o.backup = true
-o.backupdir = vim.fn.stdpath 'data' .. '/backup'
+vim.o.backup = true
+vim.o.backupdir = vim.fn.stdpath 'data' .. '/backup'
 
 -- undo
-o.undodir = vim.fn.stdpath 'data' .. '/undo'
-o.undofile = true
+vim.o.undodir = vim.fn.stdpath 'data' .. '/undo'
+vim.o.undofile = true
 
 -- swap
-o.directory = vim.fn.stdpath 'data' .. '/swap'
-o.swapfile = true
-o.updatetime = 250
+vim.o.directory = vim.fn.stdpath 'data' .. '/swap'
+vim.o.swapfile = true
+vim.o.updatetime = 250
 
 -- folding
-o.foldcolumn = '0'
-o.foldenable = true
-o.foldexpr = 'v:lua.vim.lsp.foldexpr()'
-o.foldlevel = 99
-o.foldlevelstart = 99
-o.foldmethod = 'expr'
-o.foldtext = ''
+vim.o.foldcolumn = '0'
+vim.o.foldenable = true
+vim.o.foldexpr = 'v:lua.vim.lsp.foldexpr()'
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldmethod = 'expr'
+vim.o.foldtext = ''
 
 -- text width
 local textwidth = 80
-o.textwidth = textwidth
-o.colorcolumn = textwidth .. ''
-o.cursorline = true
+vim.o.textwidth = textwidth
+vim.o.colorcolumn = textwidth .. ''
+vim.o.cursorline = true
 vim.cmd [[highlight CursorLine cterm=NONE ctermfg=NONE guifg=NONE ctermbg=NONE guibg=NONE]]
 
 -- search
-o.hlsearch = true
-o.ignorecase = true
-o.incsearch = true
-o.smartcase = true
+vim.o.hlsearch = true
+vim.o.ignorecase = true
+vim.o.incsearch = true
+vim.o.smartcase = true
 
 -- other
-o.background = 'dark'
-o.clipboard = 'unnamedplus'
-o.cmdheight = 0
-o.compatible = false
-o.errorbells = false
-o.hidden = true
-o.inccommand = 'split'
-o.modeline = false
-o.mouse = 'a'
-o.mousemodel = 'extend'
-o.pumblend = 0
-o.showcmd = true
-o.showmode = false
-o.showtabline = 1
-o.signcolumn = 'yes'
-o.spelllang = 'en_us'
-o.splitbelow = true
-o.splitright = true
-o.termguicolors = true
-o.timeout = true
-o.timeoutlen = 300
-o.visualbell = false
-o.winblend = 0
-o.wrap = false
-opt.completeopt = { 'menu', 'menuone', 'noselect' }
-opt.fillchars:append { eob = ' ' }
-opt.formatoptions:remove { 'c', 'r', 'o' }
-opt.whichwrap:append 'hl'
+vim.o.background = 'dark'
+vim.o.clipboard = 'unnamedplus'
+vim.o.cmdheight = 0
+vim.o.compatible = false
+vim.o.errorbells = false
+vim.o.hidden = true
+vim.o.inccommand = 'split'
+vim.o.modeline = false
+vim.o.mouse = 'a'
+vim.o.mousemodel = 'extend'
+vim.o.pumblend = 0
+vim.o.showcmd = true
+vim.o.showmode = false
+vim.o.showtabline = 1
+vim.o.signcolumn = 'yes'
+vim.o.spelllang = 'en_us'
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.termguicolors = true
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+vim.o.visualbell = false
+vim.o.winblend = 0
+vim.o.wrap = false
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+vim.opt.fillchars:append { eob = ' ' }
+vim.opt.formatoptions:remove { 'c', 'r', 'o' }
+vim.opt.whichwrap:append 'hl'
